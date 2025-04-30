1e69f3691edcaa4b69bea60d00f83cff4e3d11f0
Konstantsin <galajcukk@gmail.com>
2025-04-30 23:46:38 +0300

    Graph compilation 5 -> revealing node in/out types

--- Nodes2Shader/DataTypes/DataTypesConverter.cs
+++ Nodes2Shader/DataTypes/DataTypesConverter.cs
@@ -1,6 +1,4 @@
-﻿
-
-using Nodes2Shader.Compilation.MathGraph;
+﻿using Nodes2Shader.Compilation.MathGraph;
 
 namespace Nodes2Shader.DataTypes
 {
@@ -46,75 +44,84 @@ namespace Nodes2Shader.DataTypes
     {
         public static void RevealTypes(NodeData nd, List<string> variants)
         {
+            // 1. check for nd types matches with variants
 
+            // 2. if there is - cast them to variant, otherwise - cast them to greatest type
+
+            // 3. By default, output type is determined by greatest input type
         }
 
-
-
-
-
-
-
-
-
-
-        public static bool IsInterchangeable(string type1, string type2)
+        private static void CastType(ref string val, DataType typeFrom, DataType typeTo)
         {
-            if (!DataType.TryParse(type1, true, out DataType t1)) return false;
-            if (!DataType.TryParse(type2, true, out DataType t2)) return false;
+            if (typeFrom == typeTo) return;
 
-            // conversion resolved if -> equal, floating point, generic types, components amount
-            
-            // Sampler2D cannot be converted to any other type
-            if (t1 != t2 && (t1 == DataType.Sampler2D || t2 == DataType.Sampler2D)) return false;
-
-            return true;
-        }
-
-
-        private static bool IsPair(DataType t1, DataType t2, DataType r1, DataType r2, bool countOrder = false)
-        {
-            if (t1 == r1 && t2 == r2) return true;
-            if (!countOrder)
+            if (typeFrom == DataType.Float)
             {
-                if (t1 == r2 && t2 == r1) return true;
+                if (typeTo == DataType.Vec2) val = $"({val}, 0.0)";
+                else if (typeTo == DataType.Vec3) val = $"({val}, 0.0, 0.0)";
+                else if (typeTo == DataType.Vec4) val = $"({val}, 0.0, 0.0, 0.0)";
+            }
+            else if (typeFrom == DataType.Vec2)
+            {
+                if (typeTo == DataType.Float) val = val.Split(',')[0].Remove(0, 1);
+                else if (typeTo == DataType.Vec3) val = $"{val.Remove(val.Length - 2, 1)}, 0.0)";
+                else if (typeTo == DataType.Vec4) val = $"{val.Remove(val.Length - 2, 1)}, 0.0, 0.0)";
+            }
+            else if (typeFrom == DataType.Vec3)
+            {
+                if (typeTo == DataType.Float) val = val.Split(',')[0].Remove(0, 1);
+                else if (typeTo == DataType.Vec2) val = $"{val.Split(',')[0]}, {val.Split(',')[1]})";
+                else if (typeTo == DataType.Vec4) val = $"{val.Remove(val.Length - 2, 1)}, 0.0, 0.0)";
+            }
+            else if (typeFrom == DataType.Vec4)
+            {
+                if (typeTo == DataType.Float) val = val.Split(',')[0].Remove(0, 1);
+                else if (typeTo == DataType.Vec2) val = $"{val.Split(',')[0]}, {val.Split(',')[1]})";
+                else if (typeTo == DataType.Vec3) val = $"{val.Split(',')[0]}, {val.Split(',')[1]}, {val.Split(',')[2]})";
             }
 
-            return false;
+            throw new NotImplementedException($"Cast from {typeFrom} to {typeTo} is currently unavailable...");
         }
 
-        private static bool IsPair(DataType t1, DataType t2, params DataType[] rs)
+        private static DataType GetGreatestType(params DataType[] types)
         {
-            foreach (DataType r1 in rs)
-            {
-                foreach (DataType r2 in rs)
-                {
-                    if (r1 == r2) continue;
-
-                    if (IsPair(t1, t2, r1, r2)) return true;
-                }
-            }
-
-            return false;
+            return types.OrderBy(t => GetPriority(t)).Last();
         }
 
-        private static bool IsRangePair(DataType t1, DataType t2, DataType r1, DataType[] r2s)
+        private static int GetPriority(DataType type)
         {
-            foreach (DataType r2 in r2s)
-            {
-                if (IsPair(t1, t2, r1, r2)) return true;
-            }
+            if (type == DataType.Float || type == DataType.Int || type == DataType.Bool) return 1;
+            else if (type == DataType.Vec2 || type == DataType.IVec2 || type == DataType.BVec2) return 2;
+            else if (type == DataType.Vec3 || type == DataType.IVec3 || type == DataType.BVec3) return 3;
+            else if (type == DataType.Vec4 || type == DataType.IVec4 || type == DataType.BVec4) return 4;
 
-            return false;
+            return -1;
+        }
+
+        private static string GetGlslType(DataType type)
+        {
+            if (type == DataType.Vec || type == DataType.Mat ||
+                type == DataType.GenType || type == DataType.GenIType ||
+                type == DataType.GenBType || type == DataType.Any ||
+                type == DataType.Null)
+                return string.Empty;
+            else if (type == DataType.Sampler2D)
+                return "sampler2D";
+            else
+                return type.ToString().ToLower();
         }
 
         private static DataType[] GetInnerTypes(DataType type)
         {
-            if (type == DataType.GenType) return [ DataType.Float, DataType.Vec2, DataType.Vec3, DataType.Vec4 ];
-            if (type == DataType.GenIType) return [ DataType.Int, DataType.IVec2, DataType.IVec3, DataType.IVec4 ];
-            if (type == DataType.GenBType) return [ DataType.Bool, DataType.BVec2, DataType.BVec3, DataType.BVec4 ];
-            if (type == DataType.Vec) return [DataType.Vec2, DataType.Vec3, DataType.Vec4, DataType.IVec2, DataType.IVec3, DataType.IVec4, DataType.BVec2, DataType.BVec3, DataType.BVec4];
-            if (type == DataType.Mat) return [ DataType.Mat2, DataType.Mat3, DataType.Mat4, DataType.Mat2x3, DataType.Mat2x4, DataType.Mat3x2, DataType.Mat3x4, DataType.Mat4x2, DataType.Mat4x3 ];
+            if (type == DataType.GenType) return [DataType.Float, DataType.Vec2, DataType.Vec3, DataType.Vec4];
+            else if (type == DataType.GenIType) return [DataType.Int, DataType.IVec2, DataType.IVec3, DataType.IVec4];
+            else if (type == DataType.GenBType) return [DataType.Bool, DataType.BVec2, DataType.BVec3, DataType.BVec4];
+            else if (type == DataType.Vec) return [DataType.Vec2, DataType.Vec3, DataType.Vec4, DataType.IVec2, DataType.IVec3, DataType.IVec4, DataType.BVec2, DataType.BVec3, DataType.BVec4];
+            else if (type == DataType.Mat) return [DataType.Mat2, DataType.Mat3, DataType.Mat4, DataType.Mat2x3, DataType.Mat2x4, DataType.Mat3x2, DataType.Mat3x4, DataType.Mat4x2, DataType.Mat4x3];
+            else if (type == DataType.Any) return [DataType.Float, DataType.Vec2, DataType.Vec3, DataType.Vec4,
+                                                   DataType.Int, DataType.IVec2, DataType.IVec3, DataType.IVec4,
+                                                   DataType.Bool, DataType.BVec2, DataType.BVec3, DataType.BVec4,
+                                                   DataType.Mat2, DataType.Mat3, DataType.Mat4, DataType.Mat2x3, DataType.Mat2x4, DataType.Mat3x2, DataType.Mat3x4, DataType.Mat4x2, DataType.Mat4x3];
 
             return [];
         }
